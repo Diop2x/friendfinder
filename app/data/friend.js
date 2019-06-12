4442dab7d4689465968d8b7b55ce5850eec06ec8
Youssou Diop <youssoudiop@live.com>
2019-06-11 21:25:27 -0400

    DONE

---
+++ app/data/friend.js
@@ -0,0 +1,20 @@
+var friendList = [
+  {
+    name: 'Youssou Diop',
+    profilePic: 'https://avatars1.githubusercontent.com/u/45421927?s=460&v=4',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
